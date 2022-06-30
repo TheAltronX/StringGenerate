284d248596eaf6eb58365088386e1caaf60c743f
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-06-30 11:07:27 +0530

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,20 @@
+import os
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
+
+if ENVIRONMENT:
+    try:
+        API_ID = int(os.environ.get('API_ID', 0))
+    except ValueError:
+        raise Exception("Your API_ID is not a valid integer.")
+    API_HASH = os.environ.get('API_HASH', None)
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
+else:
+    # Fill the Values
+    API_ID = 0
+    API_HASH = ""
+    BOT_TOKEN = ""
+    DATABASE_URL = ""
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
---
+++ StringSessionBot/Readme.md
@@ -0,0 +1,1 @@
+Dont try to missuse this bot else this bot Automatically complain to [AltronX](https://t.me/TheAltronX)
